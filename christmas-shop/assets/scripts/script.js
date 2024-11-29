81e2d4f6096c0fe9b4ee123fc9c465f8dc612aeb
Artem_PC <taftoos2@gmail.com>
2024-11-29 17:38:00 +0300

    feat: 4 random cards are displayed, modal window style prepared

--- christmas-shop/assets/scripts/script.js
+++ christmas-shop/assets/scripts/script.js
@@ -1,17 +1,18 @@
-let gifts ;
-loadGifts();
-function loadGifts(){
-  fetch('./assets/gifts.json')
-  .then(response => {
-      if (!response.ok) {
-          throw new Error('Network response was not ok');
-      }
-      return response.json();
-  })
-  .then(res => {
-    gifts = Array.from(res);
-  })
-}
+// let gifts ;
+// loadGifts();
+import gifts from "../gifts.json" assert {type: 'json'};
+// function loadGifts(){
+//   fetch('./assets/gifts.json')
+//   .then(response => {
+//       if (!response.ok) {
+//           throw new Error('Network response was not ok');
+//       }
+//       return response.json();
+//   })
+//   .then(res => {
+//     gifts = Array.from(res);
+//   })
+// }
 //#region timer
 const daysValue =document.getElementById('days');
 const hoursValue =document.getElementById('hours');
@@ -36,12 +37,17 @@ setInterval(() => {
   calculateTime();
 }, 1000);
 //#endregion timer
+const cardsContainer = document.querySelector('.cards_container');
 
+const isHomePage = document.location.pathname.includes("home.html");
 document.addEventListener("DOMContentLoaded", function () {
-  if (document.location.pathname.includes("home.html")) {
+  if (isHomePage) {
     calculateTime();
     initSlider();
-  }
+    homeGifts();
+  }else{
+   cardsContainer.innerHTML=gifts.reduce((acc,element) => acc+draftingBlock(element),'');
+}
 });
 
 let templateWidth;
@@ -83,11 +89,12 @@ function initWindow(){
 }
 
 window.addEventListener("resize", function () {
-  slider.style.transform = `translateX(0px)`;
-  initWindow();
-  buttonLeft.classList.add('inactive');
-  buttonRight.classList.remove('inactive');
-  console.log('resized');
+  if(isHomePage){
+    slider.style.transform = `translateX(0px)`;
+    initWindow();
+    buttonLeft.classList.add('inactive');
+    buttonRight.classList.remove('inactive');
+  }
 });
 
 function sliderButtonLogic(btnL,btnR){
@@ -130,6 +137,59 @@ function sliderLogic(direction) {
   slider.style.transform=`translateX(${currentValue}px)`;
 }
 // #endregion slider
+
+//#region cards
+function pickRandom(size){
+  let cardsContent= new Set();
+  while (cardsContent.size < size) {
+    const randomIndex = Math.floor(Math.random() * gifts.length);
+    cardsContent.add(gifts[randomIndex]);
+  }
+  return Array.from(cardsContent);
+}
+
+function draftingBlock(object) {
+  let formattedCategory = object.category.replace(" ", "_").toLowerCase();
+  return `<div class="card ${formattedCategory}">
+  <div class="card-image"></div>
+  <div class="card-content">
+      <h4>${object.name}</h4>
+      <h3>${object.description}</h3>
+  </div>
+</div>`;
+}
+
+function homeGifts() {
+  while (cardsContainer.firstChild) {
+    cardsContainer.removeChild(cardsContainer.firstChild);
+  }
+  cardsContainer.innerHTML= pickRandom(4).reduce((acc,element) => acc+draftingBlock(element),'');
+}
+
+function formFields(data) {
+  const forDescription = `<h4>${data}</h4><h3>${data}</h3><p>${data}</p>`;
+  const forLive = `<p>Live</p><p>${data}</p>
+     							 <div class="stars_container">
+                  ${'<div class="star light">'.repeat(5)}
+                  </div>`;
+
+  const forCreate = `<p>Create</p><p>${data}</p>
+                  <div class="stars_container">
+                  ${'<div class="star light">'.repeat(5)}
+                  </div>`;
+
+  const forLove = `<p>Love</p><p>${data}</p>
+                  <div class="stars_container">
+                  ${'<div class="star light">'.repeat(5)}
+                  </div>`;
+
+  const forDream = `<p>Dream</p><p>${data}</p>
+                  <div class="stars_container">
+                  ${'<div class="star light">'.repeat(5)}
+                  </div>`;
+}
+//#endregion
+
 
 document.querySelector('.social.telegram').addEventListener('click', function () {
   window.open('https://web.telegram.org/', '_blank');
