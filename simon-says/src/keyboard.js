295473b0c51b4beb16a4234d2b6f894c91aee8ca
Artem_PC <taftoos2@gmail.com>
2025-01-12 18:21:49 +0300

    feat: implement rule 'Only one key can be processed at a time'

--- simon-says/src/keyboard.js
+++ simon-says/src/keyboard.js
@@ -2,13 +2,16 @@ import {game} from '../script.js';
 
 function keyboardLogic() {
 const body = document.querySelector("body");
+let firstPressed= null;
 
 body.addEventListener("keydown",e=>{
   let symbol = String.fromCharCode(e.keyCode).toLowerCase();
   let button = document.getElementById(symbol);
 
   if(button){
-    game.validateInput=symbol;
+    if(firstPressed===null){
+      firstPressed=symbol;
+    }
     button.classList.add("pressed");
     const rmMouse=()=>removeFn(button, "keyup", rmMouse);
     document.addEventListener("keyup", rmMouse);
@@ -16,9 +19,18 @@ body.addEventListener("keydown",e=>{
 });
 
 function removeFn(obj,event,operation) {
+  let symbol=obj.id;
+  let button = document.getElementById(symbol);
+
+  if(button){
+    if(firstPressed===symbol){
+      game.validateInput=symbol;
+      firstPressed=null;
+    }
   obj.classList.remove("pressed");
   document.removeEventListener(event, operation);
-};
 }
-
+}
+ 
+}
 export {keyboardLogic};
