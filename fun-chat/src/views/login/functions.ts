386807b0d9ba0e66940c4ef95935aa944e580d31
Artem_PC <taftoos2@gmail.com>
2025-04-22 20:48:43 +0300

    fix: change the value of the variable . to check the password during authorization

--- fun-chat/src/views/login/functions.ts
+++ fun-chat/src/views/login/functions.ts
@@ -2,9 +2,10 @@ import { appLogic } from '../..';
 import type { Input } from '../../modules/inputs';
 
 export function checkForm(event: Event, name: Input, password: Input): void {
+  debugger;
   const target = event.target;
   const nameLength = name.getValue().length;
-  const passwordLength = name.getValue().length;
+  const passwordLength = password.getValue().length;
   const minlength = 4;
   const maxlength = 16;
   if (target instanceof HTMLButtonElement) {
